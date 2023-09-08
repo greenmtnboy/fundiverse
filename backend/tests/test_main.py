93531e9dc4e37f44d5624aa54fe2443d25c7f76c
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-09-08 15:23:13 -0400

    test fixes

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -3,4 +3,4 @@ from fastapi.testclient import TestClient
 
 def test_read_main(test_client: TestClient):
     response = test_client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 401
