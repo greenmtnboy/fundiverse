fbd8ed13162dd6590b3d26edd5597ebe69d7a81b
efromvt <ethan.dickinson@gmail.com>
2025-03-28 08:42:34 -0400

    skip ci test

--- backend/tests/test_moomoo.py
+++ backend/tests/test_moomoo.py
@@ -1,11 +1,20 @@
+import pytest
 from py_portfolio_index.enums import ProviderType
 
 from main import LoginRequest, login, refresh_sub_portfolio
 
 
+@pytest.mark.skip(reason="Requires credentials")
 def test_moomoo():
     login(LoginRequest(key="abc", secret="123", provider=ProviderType.ALPACA))
-    login(LoginRequest(key="abc", secret="123", provider=ProviderType.MOOMOO, quote_provider=ProviderType.ALPACA))
+    login(
+        LoginRequest(
+            key="abc",
+            secret="123",
+            provider=ProviderType.MOOMOO,
+            quote_provider=ProviderType.ALPACA,
+        )
+    )
     x = refresh_sub_portfolio(
         ProviderType.MOOMOO, providers_to_refresh=[ProviderType.MOOMOO]
     )
