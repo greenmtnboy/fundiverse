3e4395ec7dbb4c0bec0952aa2c3af236ec42e360
efromvt <ethan.dickinson@gmail.com>
2025-03-28 08:20:32 -0400

    fix moomoo tests

--- backend/tests/test_moomoo.py
+++ backend/tests/test_moomoo.py
@@ -4,7 +4,8 @@ from main import LoginRequest, login, refresh_sub_portfolio
 
 
 def test_moomoo():
-    login(LoginRequest(key="abc", secret="123", provider=ProviderType.MOOMOO))
+    login(LoginRequest(key="abc", secret="123", provider=ProviderType.ALPACA))
+    login(LoginRequest(key="abc", secret="123", provider=ProviderType.MOOMOO, quote_provider=ProviderType.ALPACA))
     x = refresh_sub_portfolio(
         ProviderType.MOOMOO, providers_to_refresh=[ProviderType.MOOMOO]
     )
